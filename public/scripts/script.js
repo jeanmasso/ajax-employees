ea0f71f3fe55b26ca2cdc14864e261b93369f323
jean <jmasso@xlprog.nc>
2020-11-19 11:15:32 +1100

    maj 19/11/2020all fonctionnality good

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,48 +2,98 @@ $(document).ready(function () {
 
     getData();
 
-    function getData() {
-        $.ajax({
-            url: 'https://jsonplaceholder.typicode.com/posts',
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                for (let i = 0; i < data.length; i++) {
-                    $data = data[i];
-                    $('tbody').append(`
-                <tr>
+});
+
+//Récupération et affichage des articles globales de l'API
+function getData() {
+
+    // Requête AJAX $.ajax() de type "GET" permettant d'aller chercher les données de l'API
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // Boucle permettant de parcourir tout le tableau data
+            for (let i = 0; i < data.length; i++) {
+                $data = data[i];
+                // On insère des lignes <tr> dans la balise <tbody>
+                $('tbody').append(`
+                <!-- Ajout d'un id="row" permettant d'identifier la ligne à supprimer -->
+                <tr id="row${$data.id}">
                     <td>${$data.userId}</td>
                     <td>${$data.id}</td>
                     <td>${$data.title}</td>
                     <td>${$data.body}</td>
                     <td>
-                    <button type="button" class="btn btn-danger" id='remove'>Supprimer</button>
-                    <button type="button" onclick="`getDetailData($data.id)``" class="btn btn-secondary" data-toggle="modal" data-target="#modalShow" id="show">Afficher</button>
+                        <!-- Bouton de suppression de la ligne -->
+                        <button type="button" class="btn btn-danger" onclick="removeData($('#row${$data.id}'))">Supprimer</button>
+                        <!-- Bouton d'affichage de la modal permettant d'afficher les détails de l'article -->
+                        <button type="button" onclick="getDetailData(${$data.id})" class="btn btn-secondary" data-toggle="modal" data-target="#modalShow">Afficher</button>
                     <td>
                 <tr>
                 `);
-                };
+            };
+        }
+    });
 
-            }
-        });
 
-    }
-    
-    function getDetailData(params) {
-        console.log(params);
-        $.get('https://jsonplaceholder.typicode.com/posts/'+params,
-                function (data) {
-                    $('#modalBody').append(`
+}
+
+//Récupération et affichage des détails d'un article
+function getDetailData(params) {
+
+    // Requête AJAX $.get() permettant d'aller chercher les détails d'un article de l'API
+    $.get('https://jsonplaceholder.typicode.com/posts/' + params,
+        function (data) {
+            // On insère un paragraphe <p> dans la balise ayant pour id="modalBody"
+            $('#modalBody').html(`
                     <p>
-                        <b>User Id :</b> ${data[params].userId}</br>
-                        <b>Id :</b> ${data[params].id}</br>
-                        <b>Title :</b> ${data[params].title}</br>
-                        <b>Body :</b> ${data[params].body}</br>
+                        <b>User Id :</b> ${data.userId}</br>
+                        <b>Id :</b> ${data.id}</br>
+                        <b>Title :</b> ${data.title}</br>
+                        <b>Body :</b> ${data.body}</br>
                     </p>
                     `);
-                },'json');
-    }
+        },'json');
 
-    // $('#show')
+}
 
-})
+//Ajout d'un nouvelle article dans une nouvelle ligne
+function postNewData() {
+
+    // $userId contient la valeur de l'input ayant id="userId"
+    $userId = $('#userId').val();
+    // $title contient la valeur de l'input ayant id="title"
+    $title = $('#title').val();
+    // $body contient la valeur de l'input ayant id="body"
+    $body = $('#body').val();
+
+    // Requête AJAX $.post() permettant d'ajouter un article à l'API
+    $.post('https://jsonplaceholder.typicode.com/posts/',
+        {
+            userId: $userId,
+            title: $title,
+            body: $body,
+        }, function (data) {
+            $('tbody').append(`
+                <!-- Ajout d'un id="row" permettant d'identifier la ligne à supprimer -->
+                <tr id="row${data.id}">
+                    <td>${data.userId}</td>
+                    <td>${data.id}</td>
+                    <td>${data.title}</td>
+                    <td>${data.body}</td>
+                    <td>
+                      <button type="button" onclick="removeData($('#row${data.id}'))" class="btn btn-danger" id="remove">Supprimer</button>     
+                      <button type="button" onclick="getDetailData(${data.id})" class="btn btn-secondary" data-toggle="modal" data-target="#modalShow" id="show">Afficher</button>
+                    <td>
+                <tr>
+            `);
+        },
+    'json');
+
+}
+
+//Supppression d'une ligne
+function removeData(params) {
+    return params.remove();
+}
